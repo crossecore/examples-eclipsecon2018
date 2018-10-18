8ca5a33308fc835e70023730fda025d79805ddca
Simon Schwichtenberg <schwicht@mail.upb.de>
2018-10-18 16:42:12 +0200

    PouchDB synchronization logic added

--- angular-app/src/app/program/program.component.ts
+++ angular-app/src/app/program/program.component.ts
@@ -14,7 +14,32 @@ import PouchDB from 'pouchdb';
 import {JsonResource} from 'ecore/JsonResource';
 import {ContentTreeIterator} from 'ecore/ContentTreeIterator';
 import {EObject} from 'ecore/EObject';
+import {EPackage} from 'ecore/EPackage';
+import {EFactory} from 'ecore/EFactory';
+import {ConferencePackageImpl} from '../../conference/ConferencePackageImpl';
+import {EClassImpl} from 'ecore/EClassImpl';
+import {EClass} from 'ecore/EClass';
 
+import {EAttributeImpl} from 'ecore/EAttributeImpl';
+import {EReferenceImpl} from 'ecore/EReferenceImpl';
+import {EStructuralFeature} from 'ecore/EStructuralFeature';
+import {BasicEObjectImpl} from 'ecore/BasicEObjectImpl';
+import {AbstractCollection} from 'ecore/AbstractCollection';
+
+
+interface EObjectRegistry{
+  [index:string]:EObject;
+}
+
+interface ResolveJobRegistry{
+  [index:string]:Array<ResolveJob>;
+}
+
+interface ResolveJob{
+  eObject:EObject;
+  eStructuralFeature:EStructuralFeature;
+  value:string;
+}
 
 
 class MyAdapter implements Adapter{
@@ -43,6 +68,12 @@ export class ProgramComponent implements OnInit {
   filteredTrack:Track;
   conference: Conference;
   user:Person;
+
+  epackage:EPackage;
+  efactory:EFactory;
+
+  resolveJobs:ResolveJobRegistry;
+  eobjectRegistry:EObjectRegistry;
 
   toggleTrack(track: Track, event) {
 
@@ -74,13 +105,23 @@ export class ProgramComponent implements OnInit {
 
     this.conference = ConferenceFactoryImpl.eINSTANCE.createConference();
 
-    this.conference.eAdapters().push(new MyAdapter());
+    //this.conference.eAdapters().push(new MyAdapter());
 
     this.user = ConferenceFactoryImpl.eINSTANCE.createPerson();
+
+    this.efactory = ConferenceFactoryImpl.eINSTANCE;
+    this.epackage = ConferencePackageImpl.eINSTANCE;
+
+    this.resolveJobs = {};
+    this.eobjectRegistry = {};
 
     var closure = this;
 
     var conference = this.conference;
+
+    this.synchronize();
+
+
 
     Papa.parse('assets/eclipsecon.csv', {
       download: true,
@@ -169,14 +210,300 @@ export class ProgramComponent implements OnInit {
           closure.filteredTalks = conference.talks;
         }
 
-        closure.importIntoDataBase();
+        //closure.importIntoDataBase();
       }
+
+
 
 
     });
 
   }
 
+
+  synchronize(){
+
+    let closure = this;
+    const local_db = new PouchDB('eclipsecon');
+
+    PouchDB.sync('eclipsecon', 'http://localhost:5984/eclipsecon/', {
+      live: true,
+      retry: true
+    }).on('change', function (info) {
+
+
+      //TODO only direction 'pull'
+      info.change.docs;
+
+
+
+    }).on('paused', function (err) {
+
+      //TODO do full loading just once:
+
+
+      local_db.allDocs({
+        include_docs: true,
+        attachments: true
+      }).then(function (result) {
+
+        for(let row of result.rows){
+
+          let doc = row.doc;
+
+          let classifierId = doc['type'];
+
+          let eclassifier = closure.epackage.getEClassifier(classifierId);
+
+          if(eclassifier instanceof EClassImpl){
+
+            let eclass = eclassifier as EClass;
+
+            let eobject = closure.efactory.create(eclass);
+
+            (eobject as BasicEObjectImpl)._uuid = doc._id;
+
+            closure.eobjectRegistry[doc._id] = eobject;
+
+
+            closure.addEStructuralFeatures(eobject, doc);
+
+
+            if(closure.resolveJobs[doc._id]!==undefined){
+
+              while(closure.resolveJobs[doc._id].length>0){
+
+
+                let job = closure.resolveJobs[doc._id].pop();
+
+                if(job.eStructuralFeature.many){
+
+                  //remember: eGet call by reference
+                  let x = job.eObject.eGet(job.eStructuralFeature) as AbstractCollection<EObject>;
+                  x.add(eobject);
+                }
+                else{
+                  job.eObject.eSet(job.eStructuralFeature, eobject);
+                }
+
+
+              }
+
+            }
+          }
+
+        }
+
+        for(let uuid in closure.resolveJobs){
+
+          if(closure.resolveJobs[uuid].length>0){
+            console.log(uuid + 'not resolved: '+closure.resolveJobs[uuid].length);
+          }
+        }
+
+      }).catch(function (err) {
+        console.log(err);
+      });
+
+    }).on('active', function () {
+      // replicate resumed (e.g. new changes replicating, user went back online)
+    }).on('denied', function (err) {
+      // a document failed to replicate (e.g. due to permissions)
+    }).on('complete', function (info) {
+      // handle complete
+    }).on('error', function (err) {
+      // handle error
+    });
+
+  }
+
+  addEStructuralFeatures(eobject:EObject, document:any){
+
+    for(let key in document){
+
+      var estructuralfeature = eobject.eClass().getEStructuralFeature(key);
+
+      if (estructuralfeature instanceof EAttributeImpl)
+      {
+        if (!estructuralfeature.many)
+        {
+          var etype = estructuralfeature.eType;
+          var value = document[key];
+
+          //TODO fix nsURI
+          if (etype.ePackage.nsURI === "http://www.eclipse.org/emf/2002/Ecore" || true)
+          {
+
+            if (etype.name == "EBigDecimal")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EBigInteger")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EBoolean")
+            {
+              eobject.eSet(estructuralfeature, value == "true" ? true : false);
+            }
+            else if (etype.name == "EBooleanObject")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EByteArray")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EByteObject")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EChar")
+            {
+              eobject.eSet(estructuralfeature, value[0]);
+            }
+            else if (etype.name == "ECharacterObject")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EDateEDiagnosticChain")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EDiagnosticChain")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EDouble")
+            {
+              eobject.eSet(estructuralfeature, +value);
+            }
+            else if (etype.name == "EDoubleObject")
+            {
+              throw new Error('not implemented');
+            }
+            //EEList
+            //EEnumerator
+            //EFeatureMap
+            //EFeatureMapEntry
+            else if (etype.name == "EFloat")
+            {
+              eobject.eSet(estructuralfeature, +value);
+            }
+            else if (etype.name == "EFloatObject")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EInt")
+            {
+              eobject.eSet(estructuralfeature, +value);
+            }
+            else if (etype.name == "EIntegerObject")
+            {
+              throw new Error('not implemented');
+            }
+            //EJavaClass
+            //EJavaObject
+            else if (etype.name == "ELong")
+            {
+              eobject.eSet(estructuralfeature, +value);
+            }
+            else if (etype.name == "ELongObject")
+            {
+              throw new Error('not implemented');
+            }
+            //EMap
+            //EResource
+            //EResourceSet
+            else if (etype.name == "EShort")
+            {
+              eobject.eSet(estructuralfeature, +value);
+            }
+            else if (etype.name == "EShortObject")
+            {
+              throw new Error('not implemented');
+            }
+            else if (etype.name == "EString")
+            {
+              eobject.eSet(estructuralfeature, value);
+            }
+            //ETreeIterator
+            //EInvocationTargetException
+
+          }
+          else
+          {
+            //custom package
+
+
+          }
+
+        }
+        else if (estructuralfeature.many)
+        {
+
+        }
+        else
+        {
+
+        }
+      }
+    else if (estructuralfeature instanceof EReferenceImpl)
+      {
+
+        if(estructuralfeature.many){
+
+          var values = document[key];
+
+          for(let value of values){
+            this.resolve(eobject, estructuralfeature,value);
+          }
+
+        }
+        else{
+
+          this.resolve(eobject, estructuralfeature,document[key]);
+
+        }
+
+      }
+
+    }
+
+  }
+
+  resolve(eobject:EObject, estructuralfeature:EStructuralFeature, value:string){
+
+    if(this.eobjectRegistry[value]!==undefined){
+
+      if(estructuralfeature.many){
+
+        //TODO eGet is call by reference
+        let items = eobject.eGet(estructuralfeature) as AbstractCollection<EObject>;
+        items.add(this.eobjectRegistry[value]);
+
+      }
+      else{
+        eobject.eSet(estructuralfeature, this.eobjectRegistry[value]);
+      }
+
+
+    }
+    else{
+
+      let resolveJob: ResolveJob = {
+        "eObject": eobject,
+        "eStructuralFeature": estructuralfeature,
+        "value": value
+      };
+
+      if(this.resolveJobs[value]===undefined){
+        this.resolveJobs[value] = new Array<ResolveJob>();
+      }
+
+      this.resolveJobs[value].push(resolveJob);
+    }
+  }
 
   importIntoDataBase(){
     const db = new PouchDB('http://localhost:5984/eclipsecon/');
@@ -203,8 +530,6 @@ export class ProgramComponent implements OnInit {
       let next = treeIterator.next();
 
 
-
-
       if(next!=null){
 
         let json = jsonResource.asJson(next.value);
@@ -224,6 +549,9 @@ export class ProgramComponent implements OnInit {
     }).catch(function (err) {
       console.log(err);
     });
+
+
+
 
 
   }
